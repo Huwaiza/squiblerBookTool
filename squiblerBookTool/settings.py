9bcccf03a44ebed62fdde44f3758cd95ff225b9e
Huwaiza <muhammadhuwaizatahir@gmail.com>
2022-02-02 23:03:58 +0500

    swagger docs for apis

--- squiblerBookTool/settings.py
+++ squiblerBookTool/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'nesting_books',
+    'rest_framework',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- squiblerBookTool/urls.py
+++ squiblerBookTool/urls.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Squibler books API",
+      default_version='v1',
+      description="API endpoints for squibler book tool",
+      contact=openapi.Contact(email="muhammadhuwaizatahir@gmail.com"),
+      license=openapi.License(name="Squibler License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('books/', include('nesting_books.urls')),
 ]
